3c1d9ca422d404d52672eedc60a1762858fd66bb
Vishalreddy2020 <145963490+Vishalreddy2020@users.noreply.github.com>
2024-09-12 17:47:53 -0400

    Update rand.py

--- rand.py
+++ rand.py
@@ -1,25 +1,61 @@
 """
-This module contains a function `random_array` that fills an input list 
-with random numbers using the 'shuf' command via the subprocess module.
-"""
-import subprocess
+This module contains an implementation of the Merge Sort algorithm in Python, which is a
+divide-and-conquer sorting algorithm.
 
-def random_array(arr):
+"""
+
+import rand
+
+def merge_sort(input_arr):   # renamed 'arr' to 'input_arr'
     """
-    Fills the input array with random numbers by the 'shuf' command.
+    Recursively splits the input array into halves and sorts them using merge sort.
 
     Args:
-        arr (list): The list to be filled with random integers.
+        input_arr (list): The array to be sorted.
 
     Returns:
-        list: The input list filled with random integers.
+        list: A sorted version of the input array.
     """
-    for i, _ in enumerate(arr):
-        # Intentionally using subprocess without shell=True; no untrusted input is involved
-        # nosec: B603
-        shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
-        arr[i] = int(shuffled_num.stdout)
-    return arr
+    if len(input_arr) == 1:
+        return input_arr
+
+    half = len(input_arr) // 2
+
+    return recombine(merge_sort(input_arr[:half]), merge_sort(input_arr[half:]))
 
 
+def recombine(left_arr, right_arr):
+    """
+    Merges two sorted arrays into one sorted array.
+
+    Args:
+        left_arr (list): The first sorted array.
+        right_arr (list): The second sorted array.
+
+    Returns:
+        list: A merged and sorted array combining left_arr and right_arr.
+    """
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            right_index += 1
+            merge_arr[left_index + right_index] = left_arr[left_index]
+        else:
+            left_index += 1
+            merge_arr[left_index + right_index] = right_arr[right_index]
+
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + right_index] = right_arr[i]
+
+    for i in range(left_index, len(left_arr)):
+        merge_arr[left_index + right_index] = left_arr[i]
+
+    return merge_arr
+
+
+arr = rand.random_array([None] * 20)
+arr_out = merge_sort(arr)
+
+print(arr_out)
